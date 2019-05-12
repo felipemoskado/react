50967b1c8dcb242bc453ffa30d13828ccf5835b9
Felipe Igawa Moskado <felipe.moskado@gmail.com>
2019-05-12 18:39:04 -0300

    Extrai inputs do cadastro em novo componente

--- my-app/src/Form.js
+++ my-app/src/Form.js
@@ -1,78 +1,17 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
 
-import { saveInputValue, clearInputValue } from './store/actions';
 import './Form.scss';
+import InputField from './Input-field'
 
 class Form extends React.Component {
-  state = {
-    inputValue: ''
-  }
-
-  inputChange = event => {
-    this.setState({ inputValue: event.target.value })
-  }
-
-  keyDown = event => {
-    if (event.keyCode === KEY_CODE_ENTER) {
-      event.preventDefault();
-      this.props.saveAction(this.state.inputValue);
-    }
-  };
 
   render() {
-    const { clearAction, saveAction, storedValue } = this.props;
-    const { inputValue } = this.state;
-
     return (
       <div className="container-page-form">
-        <form className="container-form" noValidate autoComplete="off">
-          <TextField
-            label="Nome Completo"
-            value={inputValue}
-            onKeyDown={this.keyDown}
-            onChange={this.inputChange}
-          />
-          <div className="container-actions">
-            <Button 
-              className="action-save"
-              variant="outlined"
-              color="primary"
-              onClick={() => saveAction(inputValue)}
-            > 
-              Save
-            </Button>
-            <Button
-              className="action-clear"
-              variant="outlined"
-              color="primary"
-              onClick={() => clearAction()}
-              >
-                Clear
-              </Button>
-          </div>
-        </form>
-        <span className="value">Valor digitado: {storedValue}</span>
+        <InputField></InputField>
       </div>
     )
   }
 }
 
-const KEY_CODE_ENTER = 13
-
-const mapStateToProps = store => ({
-  storedValue: store.formState.inputValue
-});
-
-const mapDispatchToProps = dispatch => {
-  return {
-    saveAction: value => dispatch(saveInputValue(value)),
-    clearAction: () => dispatch(clearInputValue())
-  }
-}
-
-const form = connect(mapStateToProps, mapDispatchToProps)(Form);
-
-export default form;
+export default Form;
---
+++ my-app/src/Input-field.js
@@ -0,0 +1,112 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+import { saveInputValue, clearInputValue } from './store/actions';
+import './Form.scss';
+
+class InputField extends React.Component {
+  state = {
+    information: {
+      firstName: '',
+      lastName: '',
+      telephone: '',
+      cpf: '',
+      rg: ''
+    }
+  }
+
+  handleChange = name => event => {
+    this.setState({
+      information: {
+        ...this.state.information,
+        [name]: event.target.value
+      }
+    })
+  }
+
+  keyDown = event => {
+    if (event.keyCode === KEY_CODE_ENTER) {
+      event.preventDefault();
+      this.props.saveAction(this.state.information);
+    }
+  };
+
+  render() {
+    const { clearAction, saveAction, data } = this.props;
+    const { information } = this.state;
+
+    return (
+    <form className="container-form" noValidate autoComplete="off">
+        <TextField
+            label="First name"
+            value={information.firstName}
+            onKeyDown={this.keyDown}
+            onChange={this.handleChange('firstName')}
+        />
+        <TextField
+            label="Last name"
+            value={information.lastName}
+            onKeyDown={this.keyDown}
+            onChange={this.handleChange('lastName')}
+        />
+        <TextField
+            label="Telephone"
+            value={information.telephone}
+            onKeyDown={this.keyDown}
+            onChange={this.handleChange('telephone')}
+        />
+        <TextField
+            label="CPF"
+            value={information.cpf}
+            onKeyDown={this.keyDown}
+            onChange={this.handleChange('cpf')}
+        />
+        <TextField
+            label="RG"
+            value={information.rg}
+            onKeyDown={this.keyDown}
+            onChange={this.handleChange('rg')}
+        />
+        <div className="container-actions">
+        <Button
+            className="action-save"
+            variant="outlined"
+            color="primary"
+            onClick={() => saveAction(information)}
+        >
+            Save
+        </Button>
+        <Button
+            className="action-clear"
+            variant="outlined"
+            color="primary"
+            onClick={() => clearAction()}
+        >
+            Clear
+            </Button>
+        </div>
+        <span className="value">First name: {data.firstName}</span>
+        <span className="value">Last name: {data.lastName}</span>
+    </form>
+    )
+  }
+}
+
+const KEY_CODE_ENTER = 13
+
+const mapStateToProps = store => ({
+  data: store.formState.information
+});
+
+const mapDispatchToProps = dispatch => {
+  return {
+    saveAction: value => dispatch(saveInputValue(value)),
+    clearAction: () => dispatch(clearInputValue())
+  }
+}
+
+const inputField = connect(mapStateToProps, mapDispatchToProps)(InputField);
+
+export default inputField;
