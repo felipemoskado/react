43f2bc580273afe0c2797fce2e2f30e446da99fc
Felipe Igawa Moskado <felipe.moskado@gmail.com>
2019-06-29 20:33:25 -0300

    Utiliza a store para exibir os dados

--- pedidos/src/produtos/produto.js
+++ pedidos/src/produtos/produto.js
@@ -1,39 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import './lista-produtos.js';
 
-const Produto = () =>
-    <div className="container-horizontal lista-produtos">
-        <div className="flex container-vertical">
-            <img src={require("../images/download.jpeg")} height="200" width="100%"></img>
-            <span>Hello World</span>
-            <span>R$ 400,00</span>
-        </div>
-        <div className="flex container-vertical">
-            <img src={require("../images/download.jpeg")} height="200" width="100%"></img>
-            <span>Hello World</span>
-            <span>R$ 400,00</span>
-        </div>
-        <div className="flex container-vertical">
-            <img src={require("../images/download.jpeg")} height="200" width="100%"></img>
-            <span>Hello World</span>
-            <span>R$ 400,00</span>
-        </div>
-        <div className="flex container-vertical">
-            <img src={require("../images/gatos.png")} height="200" width="100%"></img>
-            <span>Hello World</span>
-            <span>R$ 400,00</span>
-        </div>
-        <div className="flex container-vertical">
-            <img src={require("../images/gatos.png")} height="200" width="100%"></img>
-            <span>Hello World</span>
-            <span>R$ 400,00</span>
-        </div>
-        <div className="flex container-vertical">
-            <img src={require("../images/gatos.png")} height="200" width="100%"></img>
-            <span>Hello World</span>
-            <span>R$ 400,00</span>
-        </div>
+const Produto = (props) => {
+    const { produtos } = props;
+    
+    return <div className="container-horizontal lista-produtos">
+        {produtos.map(produto => (
+            <div key={produto.id} className="flex container-vertical">
+                <img src={require(`../images/${produto.imagem}`)} height="200" width="100%"></img>
+                <span>{produto.descricao}</span>
+                <span>R$ {produto.valor}</span>
+            </div>
+        ))}
     </div>
+}
 
-export default Produto;
+const mapStateToProps = store => ({
+    produtos: store.produtoState.produtos
+});
+
+export default connect(mapStateToProps)(Produto);
